1d37ba88f7afbf90fc6f5b05f8c6e859edd51944
Mohd Firoj <firojqv@gmail.com>
2023-11-12 22:10:02 +0530

    variables practice

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountID = 12343;
+let accountEmail = "firoj@gmail.com"
+var accountPass = "643267466"
+accountCity = "Lucknow"
+//accountID = 7234;
+console.log(accountID)
+accountEmail = "firojmohd@mgail.com";
+accountPass = "hello";
+accountCity = "Gonda";
+
+console.table([accountID,accountEmail,accountPass,accountCity]);
